4df4bc3158abbe25c67b6b8b3980455130f0492e
Mark Thomas <markthethomas@gmail.com>
2015-11-19 10:23:08 -0800

    Create payments route

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
-const index = require('./routes/index');
+const payments = require('./routes/payments');
 
 const app = express();
 
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.use('/payments', payments);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
